31f19d04719017e8f67f7d9b82de2fbb2a06dccb
Parusharam <parusharam4888@gmail.com>
2023-06-05 21:34:02 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-
-// AOS.init();
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//   <App/>
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// )
-
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
