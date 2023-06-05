d8b29d8772b6c739e38069326e7648bda83262e6
Parusharam <parusharam4888@gmail.com>
2023-06-05 18:44:34 +0530

    updated with new module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+
+// import './index.css';
+// import App from './App';
+// import { BrowserRouter } from 'react-router-dom';
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+
+// AOS.init();
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//   <App/>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// )
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,9 +38,14 @@ import "aos/dist/aos.css";
 
 AOS.init();
 
-ReactDOM.render(
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>,
-  document.getElementById("root")
-)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
