080e918d8ae3f6dd7b1bafb2d12852054bf26e54
Parusharam <parusharam4888@gmail.com>
2023-06-06 09:33:25 +0530

    Updated

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+import './styles/NotFound.css'
 
-import { BiAccessibility } from "react-icons/bi";
+const NotFound = () => (
+  <div className="not-found-main-container">
+    <div className="not-found-data">
+      <img
+        src="https://res.cloudinary.com/amst/image/upload/v1639762911/notfnd_e79uve.jpg"
+        alt="not-found-pic"
+        className="not-found-image"
+      />
 
+      <h1 className="not-found-title">PAGE NOT FOUND</h1>
+      <p className="not-found-para">
+        we are sorry, the page you requested could not be found
+      </p>
 
-export default function NotFound() {
-  return (
-    <div className='content-container'>
-<h2>
-    Page Not Found
-</h2>
-
-<BiAccessibility size={45} style={{color:"red"}}/>
-<div>
-    <a href='/'>
-    <button  className='btn btn-primary'>
-        Back
-    </button>
-      </a>
-     
-</div>
+      <div className="button-container">
+        <Link to="/">
+          <button type="button" className="home-button">
+            Home
+          </button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  </div>
+)
+
+export default NotFound
